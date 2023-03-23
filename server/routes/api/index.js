ee02be1c342e27b94147aabbe94f6c4b1099da1d
Maxwell Walters <johndoe@example.com>
2023-03-23 08:25:49 -0500

    cap error

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const categoryRoutes = require('./category-routes');
 const cartRoutes = require('./cart-routes');
-const productRoutes = require('./product-routes');
+const productRoutes = require('./Product-routes');
 const orderRoutes = require('./order-routes');
 
 
